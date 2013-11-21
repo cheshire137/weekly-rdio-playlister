339cf66f05e37c9a0b7a4abc8e079b759db0ab03
Sarah Vessels <sarah.vessels@cirrusmio.com>
2013-11-20 23:16:22 -0500

    Fix Rdio login page not showing up

--- app.rb
+++ app.rb
@@ -16,6 +16,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 require 'sinatra'
+require 'logger'
 require 'omniauth'
 require 'omniauth-rdio'
 require 'json'
@@ -25,7 +26,12 @@ require 'rdio'
 RDIO_API_KEY = ENV['RDIO_API_KEY']
 RDIO_API_SHARED_SECRET = ENV['RDIO_API_SHARED_SECRET']
 
-enable :sessions
+enable :sessions, :logging
+set :session_secret, ENV['SESSION_KEY']
+
+before do
+  logger.level = Logger::DEBUG
+end
 
 use OmniAuth::Builder do
   provider :rdio, RDIO_API_KEY, RDIO_API_SHARED_SECRET
@@ -37,7 +43,6 @@ def get_client session
     client.access_token = session[:access_token]
     client.secret = session[:access_secret]
   end
-  puts client.inspect
   client
 end
 
@@ -47,13 +52,9 @@ end
 
 root_path = File.join(Dir.pwd, 'public')
 set :public_dir, root_path
-use Rack::Static, urls: ['/css', '/img', '/js'], root: root_path,
-                  index: 'index.html'
+use Rack::Static, urls: ['/css', '/img', '/js'], root: root_path
 
 get '/' do
-  puts session[:user]
-  puts session[:access_token]
-  puts session[:access_secret]
   if session[:user]
     redirect '/index.html'
   else
@@ -79,7 +80,6 @@ get '/rdio_artist_search' do
   artists = client.search(query, 'Artist')
   if artists.count > 0
     artist = artists[0]
-    puts artist.inspect
     {id: artist.key, name: artist.name}.to_json
   else
     {id: nil, error: "Could not find artist '#{query}' on Rdio."}.to_json
@@ -94,7 +94,6 @@ get '/rdio_track_search' do
   tracks = client.getTracksForArtist(artist: artist_id, query: query)
   if tracks.count > 0
     track = tracks[0]
-    puts track.inspect
     {id: track.key}.to_json
   else
     {id: nil, error: "Could not find track '#{query}' on Rdio."}.to_json
@@ -110,7 +109,6 @@ post '/rdio_playlist_create' do
   client = get_client(session)
   playlist = client.createPlaylist(name: name, description: description,
                                    tracks: tracks)
-  puts playlist.inspect
   {name: playlist.name, song_count: playlist.length, image_url: playlist.icon,
    id: playlist.key, url: playlist.shortUrl}.to_json
 end
